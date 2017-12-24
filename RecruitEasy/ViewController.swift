06e3dc8eed2c0995aafb69c19c3327b56dee177f
Daniel <daniel.shawn@outlook.com>
2017-12-24 04:31:03 -0700

    Initial Commit

---
+++ RecruitEasy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RecruitEasy
+//
+//  Created by Daniel Winn on 12/24/17.
+//  Copyright © 2017 Daniel Winn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

154fe941667cba0f05fd53ccfc6ad8c289814338
Daniel <daniel.shawn@outlook.com>
2017-12-27 06:35:10 -0700

    Convert to MVC design pattern.

--- RecruitEasy/Controllers/RecruitEasyListViewController.swift
+++ RecruitEasy/Controllers/RecruitEasyListViewController.swift
@@ -9,15 +9,28 @@
 import UIKit
 
 class RecruitEasyListViewController: UITableViewController {
+
     
-    var itemArray = ["Contact Candidate", "Screen Candidate", "Schedule Interview", "Interview Feedback", "Make Offer", "Candidate Scorecard"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Make Offer"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Phone Screen"
+        itemArray.append(newItem3)
+        
+        if let items = defaults.array(forKey: "TodoListArray") as? [Item] {
             itemArray = items
         }
         // Do any additional setup after loading the view, typically from a nib.
@@ -33,7 +46,11 @@ class RecruitEasyListViewController: UITableViewController {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "RecruitEasyItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        
+        cell.accessoryType = item.done ? .checkmark : .none
         
         return cell
     }
@@ -43,12 +60,9 @@ class RecruitEasyListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         //print(itemArray[indexPath.row])
         
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
+        tableView.reloadData()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
@@ -61,7 +75,10 @@ class RecruitEasyListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen once the user clicks the Add Item button on our UIAlert
             
-            self.itemArray.append(textField.text!)
+            let newItem = Item()
+            newItem.title = textField.text!
+            
+            self.itemArray.append(newItem)
             
             self.defaults.set(self.itemArray, forKey: "TodoListArray")
             
---
+++ RecruitEasy/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  RecruitEasy
+//
+//  Created by Daniel Winn on 12/27/17.
+//  Copyright © 2017 Daniel Winn. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
